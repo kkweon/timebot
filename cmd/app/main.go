1036f9e0047b3779fdfe826147bbbe80a3b92e87
Kyung Mo Kweon <kkweon@gmail.com>
2018-12-07 00:35:22 -0800

    Respect

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/kkweon/timebot"
@@ -21,5 +22,20 @@ func main() {
 		*token = tok
 	}
 
-	timebot.Main(*token, *debug)
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		log.Println("Unalbe to retrieve $PORT environment variable")
+		port = "8080"
+	}
+
+	go timebot.Main(*token, *debug)
+
+	http.HandleFunc("/healthcheck", root)
+	log.Println("The server is running at :" + port)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("true"))
 }
